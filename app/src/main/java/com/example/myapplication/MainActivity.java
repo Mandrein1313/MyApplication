b638beabd8aa3d09de93b3e6cb56da94fa1e4f9e
root <root@localhost>
2026-09-17 10:07:59 +0700

    Cloud Build Request - 2026-09-17 10:07:59

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,16 +1,16 @@
 package com.example.myapplication;
-synchronized
+
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-implements
+ลอง
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-Bundle
+
         BottomNavigationView nav = findViewById(R.id.bottom_nav);
         nav.setOnItemSelectedListener(item -> {
             Fragment f;
@@ -25,4 +25,4 @@ Bundle
         nav.setSelectedItemId(R.id.nav_home);
     }
 }
-myapplication
+
